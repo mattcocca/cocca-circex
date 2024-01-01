c8cc7281aea47bdf93109f85139778407e77b938
mattcocca <cocca@kairosaerospace.com>
2023-12-31 20:57:54 -0500

    Class based sensors

--- code.py
+++ code.py
@@ -13,15 +13,6 @@ DEBUG = False
 def init_thermistor():
     """Return a thermistor object that can be queried for the 
     temperature"""
-    pin = board.TEMPERATURE
-    resistor = 10000
-    resistance = 10000
-    nominal_temp = 25
-    b_coefficient = 3950
-
-    thermistor = adafruit_thermistor.Thermistor(
-        pin, resistor, resistance, nominal_temp, b_coefficient
-    )
 
     return thermistor
 
@@ -59,24 +50,63 @@ class button:
 
 
 class Sensor:
-    def __init__(self):
-        raise NotImplementedError
-    def read_sensor():
-        raise NotImplementedError
-    def serial_display():
-        raise NotImplementedError
-    def cpx_neopixel_display():
+    def __init__(self, name):
+        self.name = name
+    def read_sensor(self):
+        pass
+    def serial_display(self):
+        print("Function not yet implemented.\n\n")
+    def cpx_neopixel_display(self):
         raise NotImplementedError
     
+
 class LightSensor(Sensor):
     def __init__(self, pin):
+        self.name = "LIGHT"
         self.light = analogio.AnalogIn(pin)
         self.value = 0
-    def read_sensor():
+    def read_sensor(self):
         self.value = self.light.value
-    def serial_display():
+    def serial_display(self):
         print(str(self.value) + "\n\n")
         
+
+class TempSensor(Sensor):
+    def __init__(self):
+        self.name = "TEMP"
+        pin = board.TEMPERATURE
+        resistor = 10000
+        resistance = 10000
+        nominal_temp = 25
+        b_coefficient = 3950
+
+        self.thermistor = adafruit_thermistor.Thermistor(
+            pin, resistor, resistance, nominal_temp, b_coefficient
+        )
+    def read_sensor(self):
+        self.celsius = self.thermistor.temperature
+    def serial_display(self):
+        fahrenheit = (self.celsius * 9 / 5) + 32
+        print("{: .3f} *C\n{: .3f} *F\n".format(self.celsius,
+                                                fahrenheit))
+
+class AccelSensor(Sensor):
+    def __init__(self):
+        self.name = "ACCEL"
+        i2c = busio.I2C(board.ACCELEROMETER_SCL,
+                        board.ACCELEROMETER_SDA)
+        self.lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, address=0x19)
+        
+    def read_sensor(self):
+        self.x, self.y, self.z = [value
+                                / adafruit_lis3dh.STANDARD_GRAVITY
+                                for value in self.lis3dh.acceleration]
+
+    def serial_display(self):
+        print("x = {: .3f} G\ny = {: .3f} G\nz = {: .3f} G".format(
+                                                                self.x,
+                                                                self.y,
+                                                                self.z))
 
 def rwd_lines(lines):
     # TODO: Support variable number of output lines
@@ -85,50 +115,35 @@ def rwd_lines(lines):
 
 
 if __name__ == "__main__":
-    MODE_DICT = {"LIGHT": LightSensor(board.A8)}
-    MODE_LIST = ["TEMP", "GYRO", "SOUND", "LIGHT"]
+    MODE_LIST = [
+                TempSensor(),
+                LightSensor(board.A8),
+                AccelSensor(),
+                Sensor("SOUND")
+                ]
+
     mode_current=0
     record_state = False
     
     a_button = button(board.D4)
     b_button = button(board.D5)
 
-    light = analogio.AnalogIn(board.A8)
-    thermistor = init_thermistor()
-    accel = init_accel()
-
     print("\033[?25l")
-    print("Mode: " + MODE_LIST[mode_current] + "\n\n\n")
+    print("Mode: " + MODE_LIST[mode_current].name + "\n\n\n")
     while True:
         if (a_button.pressed()): 
             mode_current = (mode_current + 1) % len(MODE_LIST)
             rwd_lines(4)
-            print("Mode: " + MODE_LIST[mode_current] + "\n\n\n")
+            print("Mode: " + MODE_LIST[mode_current].name + "\n\n\n")
 
         if (b_button.pressed()):
             record_state = ~record_state
 
         if record_state:
             rwd_lines(3)
-
-            if MODE_LIST[mode_current] == "TEMP":
-                celsius = thermistor.temperature
-                fahrenheit = (celsius * 9 / 5) + 32
-                print("{: .3f} *C\n{: .3f} *F\n"
-                        .format(celsius, fahrenheit))
-
-            elif MODE_LIST[mode_current] == "LIGHT":
-                print(str(light.value) + "\n\n")
-
-            elif MODE_LIST[mode_current] == "GYRO":
-                x, y, z = [value / adafruit_lis3dh.STANDARD_GRAVITY 
-                    for value in accel.acceleration]
-                print("x = {: .3f} G\ny = {: .3f} G\nz = {: .3f} G"
-                        .format(x,y,z))
-
-            else:
-                print("Function not yet implemented\n\n")
-
+        
+            MODE_LIST[mode_current].read_sensor()
+            MODE_LIST[mode_current].serial_display()
         else:
             rwd_lines(3)
             print("Press B to start recording\n\n")
