d3069502fcb03d49552d36032c54732a8d8c030c
mattcocca <cocca@kairosaerospace.com>
2024-01-04 23:26:33 -0700

    Specify device target

--- copy-n-run.sh
+++ copy-n-run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 if [ $1 ]; then
-    TARGET_DIR=$1
+    TARGET_DEV=$1
+    echo "Target device specified as ${TARGET_DEV}"
+else
+    echo "No target device specified, exiting..."
+    exit 1
+fi;
+
+if [ $2 ]; then
+    TARGET_DIR=$2
     echo "Code target specifed as ${TARGET_DIR}"
 else
     TARGET_DIR=/Volumes/CIRCUITPY/
@@ -12,4 +20,4 @@ echo "Writing code.py to ${TARGET_DIR}"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cp ${SCRIPT_DIR}/code.py $TARGET_DIR
-screen /dev/tty.usbmodem2101 115200
+screen ${TARGET_DEV} 115200
